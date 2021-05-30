99d03f5d760168bf0c866118a13f219a481a8073
Nicole Klotzly <nikki@DESKTOP-TTLI929.(none)>
2021-05-30 16:32:05 -0700

    Added user posts and bio to profile pages

--- static/js/profile.js
+++ static/js/profile.js
@@ -9,30 +9,13 @@ let init = (app) => {
 
     // This is the Vue data.
     app.data = {
+        post_list: [],
+        user: -1,
         thumbnail: "",
-        //thumbnail_list: [],
+        bio: "",
+        edit_bio: false,
+        new_bio: "",
     };
-
-    /*app.enumerate = (a) => {
-        // This adds an _idx field to each element of the array.
-        let k = 0;
-        a.map((e) => {e._idx = k++;});
-        return a;
-    };*/
-
-    /*app.add_thumbnail = function(){
-        axios.post(add_thumbnail_url,{
-            thumbnail: app.vue.thumbnail
-        })
-        .then(function(response){
-            app.vue.thumbnail_list.push({
-                id: response.data.id,
-                email: response.data.email,
-                thumbnail: app.vue.thumbnail
-            });
-        });
-        app.vue.thumbnail = "";
-    }*/
 
     app.upload_file = function(event){
         let input = event.target; //reference to input object that triggers event
@@ -44,46 +27,58 @@ let init = (app) => {
                 app.vue.thumbnail = reader.result;
                 axios.post(upload_thumbnail_url, {
                     tl: app.vue.thumbnail
-                });//.then(function(response){
-                    //set local preview
-                    //app.vue.thumbnail=reader.result;
-                    /*
-                    app.vue.thumbnail_list.push({
-                        id: response.data.id,
-                        email: response.data.email,
-                        thumbnail: app.vue.thumbnail,
-                    });*/
                 });
+            });
 
             reader.readAsDataURL(file);
-            }
-            //app.vue.thumbnail = "";
-
-            /*app.vue.uploading = true;
-            let file_type = file.type;
-            let file_name = file.name;
-            let full_url = file_upload_url + "&file_name=" + encodeURIComponent(file_name)
-                + "&file_type=" + + encodeURIComponent(file_type);
-            let req = new XMLHttpRequest();
-            req.addEventListener("load", function(){
-                app.upload_complete(file_name, file_type);
-            });
-            req.open("PUT", full_url, true);
-            req.send(file); //uploads file to website*/
-        };
-    //};
+        }
+    };
 
     app.delete_profilepic = function(){
         axios.post(delete_profilepic_url);
         app.vue.thumbnail = "";
     };
 
+
+    app.delete_post = function(p_idx) {
+        let id = p_idx;
+        axios.get(delete_post_url, {params: {id: id}})
+        .then(function (response) {
+            for (let i = 0; i < app.vue.post_list.length; i++) {
+                if (app.vue.post_list[i].id === id) {
+                    app.vue.post_list.splice(i, 1);
+                    break;
+                }
+            }
+        });
+    };
+
+    app.set_edit_bio = function(new_value){
+        app.vue.edit_bio = new_value;
+    }
+
+    app.add_bio = function(){
+        app.vue.bio = app.vue.new_bio;
+        axios.post(add_bio_url,{
+            bio: app.vue.bio
+        });
+        app.cancel_bio();
+    }
+
+    app.cancel_bio = function(){
+        app.vue.new_bio = "";
+        app.vue.edit_bio = false;
+    }
+
     // This contains all the methods.
     app.methods = {
         // Complete as you see fit.
         upload_file: app.upload_file,
         delete_profilepic: app.delete_profilepic,
-        //add_thumbnail: app.add_thumbnail,
+        delete_post: app.delete_post,
+        add_bio: app.add_bio,
+        cancel_bio: app.cancel_bio,
+        set_edit_bio: app.set_edit_bio,
     };
 
     // This creates the Vue instance.
@@ -99,10 +94,35 @@ let init = (app) => {
         // Typically this is a server GET call to load the data.
         axios.get(get_profile_url)
         .then(function(result){
-            //let users = result.data.users;
-            //app.enumerate(users);
             app.vue.thumbnail = result.data.tl;
+            app.vue.user = result.data.uid;
+            app.vue.bio = result.data.bio;
         });
+
+        axios.get(get_posts_url)
+        .then(function(result){
+            let posts = result.data.posts;
+            for(let i = 0; i < posts.length; i++) {
+                if(posts[i].user == app.vue.user){ //if user id from post matches current user id
+                    app.vue.post_list.push({
+                        id: posts[i].id,
+                        title: posts[i].title,
+                        post_text: posts[i].post_text,
+                        image: posts[i].image,
+                        username: posts[i].username,
+                        email: posts[i].email,
+                        country: posts[i].country,
+                        overall: posts[i].overall,
+                        beach: posts[i].beach,
+                        sights: posts[i].sights,
+                        food: posts[i].food,
+                        night: posts[i].night,
+                        shop: posts[i].shop,
+                    });
+                }
+            }
+        });
+
     };
 
     // Call to the initializer.
